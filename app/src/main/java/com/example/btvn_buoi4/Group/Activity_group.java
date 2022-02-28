ce3b255f9fa1df3c497d5c11178a97257fac33b8
Duy Le <lehongduy.bk@gmail.com>
2022-02-28 19:27:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/btvn_buoi4/Group/Activity_group.java
@@ -0,0 +1,63 @@
+package com.example.btvn_buoi4.Group;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.btvn_buoi4.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Activity_group extends AppCompatActivity {
+    private RecyclerView rcv_group;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+        rcv_group = findViewById(R.id.rcv_group);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplication(), LinearLayoutManager.VERTICAL, false);
+        GroupAdapter adapter = new GroupAdapter(setListData());
+        rcv_group.setLayoutManager(linearLayoutManager);
+        rcv_group.setAdapter(adapter);
+
+        setToolbar();
+
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.icon_back);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        getWindow().setStatusBarColor(getColor(R.color.white));
+
+    }
+
+    private List<GroupModel> setListData() {
+        List<GroupModel> groupModelList = new ArrayList<>();
+
+        groupModelList.add(new GroupModel(R.drawable.martphone, "Mua bán có tâm", 8, 10, GroupModel.STATUS_POST.NHOM_DONG));
+        groupModelList.add(new GroupModel(R.drawable.eat_clean, "Ăn để lăn", 11, 10, GroupModel.STATUS_POST.NHOM_KIN));
+        groupModelList.add(new GroupModel(R.drawable.baby, "Chia sẽ kiến thức tài liệu Montessori", 8, 10, GroupModel.STATUS_POST.NHOM_MO));
+        groupModelList.add(new GroupModel(R.drawable.eat_clean_2, "Thực đơn Eat-Clean giảm cân khỏe đẹp", 8, 10, GroupModel.STATUS_POST.NHOM_MO));
+        groupModelList.add(new GroupModel(R.drawable.kento, "Easy kento for busy People", 8, 8, GroupModel.STATUS_POST.NHOM_KIN));
+        groupModelList.add(new GroupModel(R.drawable.car, "OFBB", 8, 10, GroupModel.STATUS_POST.NHOM_MO));
+        groupModelList.add(new GroupModel(R.drawable.eat_clean_2, "Thực đơn Eat-Clean giảm cân khỏe đẹp", 11, 10, GroupModel.STATUS_POST.NHOM_MO));
+        groupModelList.add(new GroupModel(R.drawable.kento, "Easy kento for busy People", 8, 10, GroupModel.STATUS_POST.NHOM_KIN));
+
+        return groupModelList;
+    }
+}
